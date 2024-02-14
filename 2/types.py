f70d97250a4fef11f8c61445535ed990396311b8
Anton Ozerov <98531046+AlexMarticus@users.noreply.github.com>
2024-02-14 18:12:54 +0300

    Create types.py

---
+++ 2/types.py
@@ -0,0 +1,21 @@
+# Присвойте переменной целое число и получите вывод <class 'int'>.
+current_month =
+type_var = type(current_month)
+print(type_var)
+# требуемый вывод:
+# <class 'int'>
+
+# Исправьте ошибки в коде, что бы получить требуемый вывод.
+polar_radius_title = "Радиус" "Земли"
+polar_radius_float = 6378,1 
+print(polar_radius_title, polar_radius_float)
+# требуемый вывод:
+# Радиус Земли 6378.1
+
+
+# Создайте переменные, что бы получить требуемый вывод.
+str_type, int_type, float_type, bool_type = 
+print("Типы данных: ", end="")
+print(type(str_type), type(int_type, type(float_type), type(bool_type), sep=",")
+# требуемый вывод:
+# Типы данных: <class 'str'>, <class 'int'>, <class 'float'>, <class 'bool'>
