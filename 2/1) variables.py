443acd8c790744a87ccbc8571b29879542df4f22
minty209 <160029451+minty209@users.noreply.github.com>
2024-02-20 18:14:11 +0400

    Update 1) variables.py

--- 2/1) variables.py
+++ 2/1) variables.py
@@ -23,3 +23,10 @@ print(d)
 print("Мой стек:", programming_lang_1, programming lang_2, programming_lang_3)
 # требуемый вывод:
 # Мой стек: python, javascript, php
+# Создайте переменные, что бы вывести требуемый текст.
+programming_lang_1 = ['python']
+programming_lang_2 = ['javascript']
+programming_lang_3 = ['php']
+print("Мой стек:", programming_lang_1, programming_lang_2, programming_lang_3)
+# требуемый вывод:
+# Мой стек: python, javascript, php
