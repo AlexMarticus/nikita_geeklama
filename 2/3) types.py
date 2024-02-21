fb4f4478520fcf6c9c3bba0fa9b7a0bdb345bd96
minty209 <160029451+minty209@users.noreply.github.com>
2024-02-21 18:13:59 +0400

    Update 3) types.py

--- 2/3) types.py
+++ 2/3) types.py
@@ -1,44 +1,37 @@
+#кортеж занимает намного меньше памяти чем список
+
 # Присвойте переменной целое число и получите вывод <class 'int'>.
-current_month =
+current_month = "4"
 type_var = type(current_month)
 print(type_var)
 # требуемый вывод:
 # <class 'int'>
 
 # Исправьте ошибки в коде, что бы получить требуемый вывод.
-polar_radius_title = "Радиус" "Земли"
-polar_radius_float = 6378,1 
+polar_radius_title = "Радиус Земли"
+polar_radius_float = 6378.1
 print(polar_radius_title, polar_radius_float)
 # требуемый вывод:
 # Радиус Земли 6378.1
 
-
 # Создайте переменные, что бы получить требуемый вывод.
-str_type, int_type, float_type, bool_type = 
-print("Типы данных: ", end="")
-print(type(str_type), type(int_type, type(float_type), type(bool_type), sep=",")
+str_type, int_type, float_type, bool_type = "100", 2, 2.5, True
+print("Типы данных: ", end="\n")
+print(type(str_type), type(int_type), type(float_type), type(bool_type), sep=" - ")
 # требуемый вывод:
 # Типы данных: <class 'str'>, <class 'int'>, <class 'float'>, <class 'bool'>
 
+
 # Измените и дополните код, что бы переменная salary_type хранила результат функции type() со значением int.
-salary = "50000"
-salary_type = 
+salary = '50000'
+salary_type = type(int(salary))
 print(salary_type)
 # требуемый вывод:
 # <class 'int'>
 
-
 # Исправьте ошибку в коде, что бы получить требуемый вывод.
 # данный код
-mark = int("5+")
-print(mark, "баллов")
+mark = int("5")
+print(f"{mark}+ баллов")
 # требуемый вывод:
 # 5+ баллов
-
-# Конвертируйте переменные и введите только целые числа через дефис.
-score1 = 50.5648
-score2 = 23.5501
-score3 = 96.560
-print()
-# требуемый вывод:
-# 50-23-96
